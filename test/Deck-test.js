3ee5c7647083323d421b0ea46e595f4f3098c918
Kara Anderson <kanderson8585@gmail.com>
2023-01-31 08:58:17 -0700

    Add beforeEach to test files

---
+++ test/Deck-test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', () => {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  
+  beforeEach(() => {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(8, 'What do iterator methods take in as their first argument?', ['callback function', 'current element', 'an array'], 'callback function');
+    card3 = new Card(16, 'What does the callback function for reduce() return?', ['the accumulator', 'the current element', 'the initializer'], 'the accumulator');
+    deck = new Deck([card1, card2, card3]);
+  });
+  // should be a function
+  // should be an instance of Deck
+  it('should contain a deck of cards', () => {
+    expect(deck.cards).to.be.an('array');
+  });
+
+  it('should be able to return the number of cards', () => {
+    const deckSize = deck.countCards();
+
+    expect(deckSize).to.equal(3);
+  });
+});

25f9eb82c603a4dc8e1d455eafd6caba2c3e75a8
Kara Anderson <kanderson8585@gmail.com>
2023-02-01 15:57:32 -0700

    Pass all Game tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,18 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Deck = require('../src/Deck');
 const Card = require('../src/Card');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
 const Game = require('../src/Game');
+const data = require('../src/data');
 
 describe('Game', () => {
-  let deck, game;
+  let game;
 
   beforeEach(() => {
-    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(8, 'What do iterator methods take in as their first argument?', ['callback function', 'current element', 'an array'], 'callback function');
-    const card3 = new Card(16, 'What does the callback function for reduce() return?', ['the accumulator', 'the current element', 'the initializer'], 'the accumulator');
-    deck = new Deck([card1, card2, card3]);
     game = new Game();
   });
 
@@ -24,17 +22,32 @@ describe('Game', () => {
     expect(game).to.be.an.instanceOf(Game);
   });
 
-  it.skip('should start with round one', () => {
+  it('should start with no rounds played', () => {
+    expect(game.currentRound).to.equal(0);
+  });
+
+  it('should be able to start the game', () => {
+    expect(game.start).to.be.a('function');
+  });
+
+  it('should create a deck of cards for the game', () => {
+    const deck = game.createCards();
+
+    expect(game.deck).to.be.an.instanceOf(Deck);
+    expect(game.deck.cards.length).to.equal(30);
+  });
+
+  it('should start a new round', () => {
+    game.createCards();
+    game.beginRound();
+
     expect(game.currentRound).to.equal(1);
+    expect(game.round).to.be.an.instanceOf(Round);
   });
 
-  it.skip('should be able to start the game', () => {
-    const beginning = game.start();
+  it('should have a deck full of card objects', () => {
+    game.createCards();
 
-    expect(beginning).to.be.a('function');
-  });
-
-  it.skip('should create a set of cards for the game', () => {
-    
-  });
+    expect(game.deck.cards[0]).to.be.an.instanceOf(Card);
+  })
 });
